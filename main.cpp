a4eb0c4fa06683a9a4945e94cacd5b99259d614f
BareLinux <contact@barelinux.io>
2023-10-09 21:35:12 +0300

    Updated namespace

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <sstream>
 #include <vector>
 
-namespace fs = std::filesystem;
+namespace list = std::filesystem; // Updated namespace to 'list'
 
 // ANSI escape codes for text colors
 const std::string GREEN = "\x1B[32m";  // Green
@@ -12,21 +12,21 @@ const std::string DARK_BLUE = "\x1B[34m";  // Dark Blue
 const std::string CYAN = "\x1B[36m";  // Cyan
 const std::string RESET_COLOR = "\x1B[0m";  // Reset color to default
 
-std::uintmax_t calculateDirectorySize(const fs::path& directoryPath) {
+std::uintmax_t calculateDirectorySize(const list::path& directoryPath) { // Updated 'fs::path' to 'list::path'
     std::uintmax_t totalSize = 0;
 
-    for (const auto& entry : fs::recursive_directory_iterator(directoryPath)) {
-        const fs::path& path = entry.path();
+    for (const auto& entry : list::recursive_directory_iterator(directoryPath)) { // Updated 'fs::recursive_directory_iterator' to 'list::recursive_directory_iterator'
+        const list::path& path = entry.path(); // Updated 'fs::path' to 'list::path'
 
-        if (fs::is_regular_file(path)) {
-            totalSize += fs::file_size(path);
+        if (list::is_regular_file(path)) { // Updated 'fs::is_regular_file' to 'list::is_regular_file'
+            totalSize += list::file_size(path); // Updated 'fs::file_size' to 'list::file_size'
         }
     }
 
     return totalSize;
 }
 
-bool compareEntries(const fs::directory_entry& entry1, const fs::directory_entry& entry2) {
+bool compareEntries(const list::directory_entry& entry1, const list::directory_entry& entry2) { // Updated 'fs::directory_entry' to 'list::directory_entry'
     if (entry1.is_directory() && !entry2.is_directory()) {
         return true;
     } else if (!entry1.is_directory() && entry2.is_directory()) {
@@ -36,20 +36,20 @@ bool compareEntries(const fs::directory_entry& entry1, const fs::directory_entry
     }
 }
 
-void listDirectoryContents(const fs::path& directoryPath) {
-    std::vector<fs::directory_entry> entries;
+void listDirectoryContents(const list::path& directoryPath) { // Updated 'fs::path' to 'list::path'
+    std::vector<list::directory_entry> entries; // Updated 'fs::directory_entry' to 'list::directory_entry'
 
-    for (const auto& entry : fs::directory_iterator(directoryPath)) {
+    for (const auto& entry : list::directory_iterator(directoryPath)) { // Updated 'fs::directory_iterator' to 'list::directory_iterator'
         entries.push_back(entry);
     }
 
     std::sort(entries.begin(), entries.end(), compareEntries);
 
     for (const auto& entry : entries) {
-        const fs::path& path = entry.path();
+        const list::path& path = entry.path(); // Updated 'fs::path' to 'list::path'
         std::string size;
 
-        if (fs::is_directory(path)) {
+        if (list::is_directory(path)) { // Updated 'fs::is_directory' to 'list::is_directory'
             std::uintmax_t dirSize = calculateDirectorySize(path);
             if (dirSize == 0) {
                 size = "Empty";
@@ -69,8 +69,8 @@ void listDirectoryContents(const fs::path& directoryPath) {
 
             // Color code for folders (dark blue)
             std::cout << DARK_BLUE << path.filename().string() << RESET_COLOR << " (" << size << ")" << std::endl;
-        } else if (fs::is_regular_file(path)) {
-            std::uintmax_t fileSize = fs::file_size(path);
+        } else if (list::is_regular_file(path)) { // Updated 'fs::is_regular_file' to 'list::is_regular_file'
+            std::uintmax_t fileSize = list::file_size(path); // Updated 'fs::file_size' to 'list::file_size'
             if (fileSize < 1024 * 1024) {
                 size = std::to_string(fileSize / 1024) + " KB";
             } else if (fileSize < 1024 * 1024 * 1024) {
@@ -97,11 +97,11 @@ void listDirectoryContents(const fs::path& directoryPath) {
 }
 
 int main(int argc, char* argv[]) {
-    fs::path pathToList;
+    list::path pathToList; // Updated 'fs::path' to 'list::path'
 
     if (argc == 1) {
         // No command-line arguments provided, list the current directory.
-        pathToList = fs::current_path();
+        pathToList = list::current_path(); // Updated 'fs::current_path' to 'list::current_path'
     } else if (argc == 2) {
         // Command-line argument provided, use it as the path to list.
         pathToList = argv[1];
@@ -110,17 +110,17 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    if (!fs::exists(pathToList)) {
+    if (!list::exists(pathToList)) { // Updated 'fs::exists' to 'list::exists'
         std::cerr << "Path does not exist: " << pathToList.string() << std::endl;
         return 1;
     }
 
     std::cout << "Listing contents of: " << pathToList.string() << std::endl;
 
-    if (fs::is_directory(pathToList)) {
+    if (list::is_directory(pathToList)) { // Updated 'fs::is_directory' to 'list::is_directory'
         listDirectoryContents(pathToList);
-    } else if (fs::is_regular_file(pathToList)) {
-        std::uintmax_t fileSize = fs::file_size(pathToList);
+    } else if (list::is_regular_file(pathToList)) { // Updated 'fs::is_regular_file' to 'list::is_regular_file'
+        std::uintmax_t fileSize = list::file_size(pathToList); // Updated 'fs::file_size' to 'list::file_size'
         std::string size;
 
         if (fileSize < 1024 * 1024) {
